ecd168618a35f731368b62af1c5869ff750bc8a9
Tristan Hampton <tristanhampton@icloud.cmo>
2019-11-22 14:33:17 -0700

    Display Update
    Updated to display three items across on front-page.php.
    Removed PS4 and Switch tag sections on front-page.php

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 <main class="front-page">
 
 <!-- RECENTLY ADDED GAMES -->
-  <?php $query = new WP_Query(array('post_type'=>'game', 'posts_per_page'=>2)) ?>
+  <?php $query = new WP_Query(array('post_type'=>'game', 'posts_per_page'=>3)) ?>
   <?php if($query -> have_posts()): ?>
   <section>
     <h2>Recently Added</h2>
@@ -21,39 +21,7 @@
   </section>
   <?php endif ?>
 
-<!-- PS4 GAMES -->
-  <?php $query = new WP_Query(array('tag'=>'PS4', 'post_type'=>'game', 'posts_per_page'=>2)) ?>
-  <?php if($query -> have_posts()): ?>
-  <section>
-    <h2>PS4 Games</h2>
-    <?php while($query -> have_posts()): $query -> the_post(); ?>
-      <?php $header = get_field('header'); ?>
-      <div class="thumbnail">
-        <a href="<?php the_permalink() ?>">
-          <h3><?php echo $header['game_title'] ?></h3>
-          <img src="<?php echo $header['cover'] ?>" alt="<?php echo $header['game_title'] ?> cover">
-        </a>
-      </div>
-    <?php endwhile ?>
-  </section>
-  <?php endif ?>
 
-<!-- SWITCH GAMES -->
-  <?php $query = new WP_Query(array('tag'=>'Switch', 'post_type'=>'game', 'posts_per_page'=>2)) ?>
-  <?php if($query -> have_posts()): ?>
-  <section>
-    <h2>Switch Games</h2>
-    <?php while($query -> have_posts()): $query -> the_post(); ?>
-      <?php $header = get_field('header'); ?>
-      <div class="thumbnail">
-        <a href="<?php the_permalink() ?>">
-          <h3><?php echo $header['game_title'] ?></h3>
-          <img src="<?php echo $header['cover'] ?>" alt="<?php echo $header['game_title'] ?> cover">
-        </a>
-      </div>
-    <?php endwhile ?>
-  </section>
-  <?php endif ?>
 
 <!-- GAME REVIEWS -->
   <?php $query = new WP_Query(array('post_type'=>'review', 'posts_per_page'=>2)) ?>
